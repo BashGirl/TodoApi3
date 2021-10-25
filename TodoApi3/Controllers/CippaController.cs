996c5f6b69d6c73c70890a4d4ef917d2e8fd710e
Melissa Passeri <melissapasseri@gmail.com>
2021-10-25 23:35:12 +0200

    Install MediatR #WIP

---
+++ TodoApi3/Controllers/CippaController.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace TodoApi3.Controllers
+{
+    public class CippaController
+    {
+        public CippaController()
+        {
+        }
+    }
+}
---
+++ TodoApi3/Models/Mediator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace TodoApi3.Models
+{
+    public class Mediator : IMediator
+    {
+        private ServiceFactory serviceFactory { get; }
+
+
+
+        public Mediator(ServiceFactory ServiceFactory)
+        {
+            serviceFactory = ServiceFactory;
+        }
+
+
+
+
+        public Task Publish(object notification, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Publish<TNotification>(TNotification notification, CancellationToken cancellationToken = default) where TNotification : INotification
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TResponse> Send<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<object> Send(object request, CancellationToken cancellationToken = default)
+        {
+            serviceFactory.BeginInvoke();
+            throw new NotImplementedException();
+        }
+    }
+}
